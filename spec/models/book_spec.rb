936091aa3b7cc459107551b06ee2b8e3e66c8fe3
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-04 14:52:05 +0900

    Using contexts on model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  describe "Book#title_with_author" do # describeメソッドをつかってメソッドごとに区切ると読みやすいです
-    it "Book#title_with_authorを呼び出したとき、titleとauthorを結んだ文字列が返ること" do
+  context "Book#titleが文字列のとき" do # 状況を説明する
+    it "titleとauthorを結んだ文字列が返ること" do
       book = Book.new(title: "RubyBook", author: "matz")
       expect(book.title_with_author).to eq("RubyBook - matz")
     end
-  end
-
-  it "Bookモデルをnewしたとき、nilではないこと" do
-    expect(Book.new).not_to eq(nil)
   end
 end
