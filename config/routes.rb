47601f291ada5c7dbee83d12b416b9423074c98c
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-04 15:31:08 +0900

    Write Request spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :books
+  get "status" => "status#index", defaults: { format: "json" }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
