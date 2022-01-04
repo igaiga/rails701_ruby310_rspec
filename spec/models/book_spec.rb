4ff9c7fd64fdf3db8c1a5fd791664e700014e59a
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-04 15:40:45 +0900

    Use mocks

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  context "Book#titleが文字列のとき" do # 状況を説明する
-    it "titleとauthorを結んだ文字列が返ること" do
-      book = Book.new(title: "RubyBook", author: "matz")
-      expect(book.title_with_author).to eq("RubyBook - matz")
+  describe "Book#title" do
+    context "Book#titleが文字列のとき" do # 状況を説明する
+      it "titleとauthorを結んだ文字列が返ること" do
+        book = Book.new(title: "RubyBook", author: "matz")
+        expect(book.title_with_author).to eq("RubyBook - matz")
+      end
+    end
+
+    context "Book#titleがnilのとき" do # 状況を説明する
+      it "空のtitleとauthorを結んだ文字列が返ること" do
+        book = Book.new(author: "matz")
+        expect(book.title_with_author).to eq(" - matz")
+      end
     end
   end
-  context "Book#titleがnilのとき" do # 状況を説明する
-    it "空のtitleとauthorを結んだ文字列が返ること" do
-      book = Book.new(author: "matz")
-      expect(book.title_with_author).to eq(" - matz")
+
+  describe "Book#bonus" do
+    context "lucky?がtrueのとき" do
+      it "チェキが返ること" do
+        book = Book.new
+        allow(book).to receive(:lucky?).and_return(true)
+        expect(book).to receive(:lucky?) # 確認するメソッド呼び出しを実行する前に書く
+        expect(book.bonus).to eq("著者サイン入りチェキ")
+      end
     end
   end
 end
