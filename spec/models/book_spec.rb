08c0b386c153db002a721a0120a536749d003ce5
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-04 14:52:05 +0900

    Using contexts on model spec

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,10 @@ RSpec.describe Book, type: :model do
       expect(book.title_with_author).to eq("RubyBook - matz")
     end
   end
+  context "Book#titleがnilのとき" do # 状況を説明する
+    it "空のtitleとauthorを結んだ文字列が返ること" do
+      book = Book.new(author: "matz")
+      expect(book.title_with_author).to eq(" - matz")
+    end
+  end
 end
