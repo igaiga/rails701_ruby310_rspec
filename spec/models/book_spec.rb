8d0f905be18fa40f63de8ec7690e2b8b5825c106
Kuniaki Igarashi <igaiga@gmail.com>
2022-01-07 09:49:28 +0900

    Writing model specs

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Book#title_with_author" do # describeメソッドをつかってメソッドごとに区切ると読みやすいです
+    it "Book#title_with_authorを呼び出したとき、titleとauthorを結んだ文字列が返ること" do
+      book = Book.new(title: "RubyBook", author: "matz")
+      expect(book.title_with_author).to eq("RubyBook - matz")
+    end
+  end
 end
